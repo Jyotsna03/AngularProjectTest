54876d5c64db7429b6a67cf79b1718f6d6b59b42
Jyotsna03 <jyotsnabh03@gmail.com>
2025-01-25 20:33:02 +0530

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    TableComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
